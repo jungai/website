5e02f958237282762bf354da3770bbe94cfb6f42
jungai <juworaphol125@gmail.com>
2022-06-26 16:09:34 +0700

    feat: add unocss

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import Unocss from 'unocss/vite';
+import presetWind from '@unocss/preset-wind';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +10,14 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		vite: {
+			plugins: [
+				Unocss({
+					presets: [presetWind()]
+				})
+			]
+		}
 	}
 };
 
