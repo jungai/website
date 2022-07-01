c6dc524c39f17a3b58a7250289efa5ae6eea5887
jungai <juworaphol125@gmail.com>
2022-07-01 22:09:51 +0700

    feat: add sidebar & header

---
+++ apps/docs/src/lib/components/Header.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import FaGithubAlt from 'svelte-icons/fa/FaGithubAlt.svelte';
+</script>
+
+<header
+	class="fixed border-b border-[#34495e] w-full p-4 shadow-md flex justify-center items-center h-[60px] z-50 bg-white"
+>
+	<div class="w-3/5 flex justify-between items-center">
+		<h1 class="text-xl font-semibold">
+			<span class="font-semibold text-[#1abc9c]">J</span>ungai Doc<span
+				class="font-semibold text-[#1abc9c]">s</span
+			>
+		</h1>
+		<ul class="flex gap-4 items-center justify-center">
+			<li class="hover:text-[#9b59b6] hover:font-semibold hover:italic cursor-pointer">Blog</li>
+			<li class="hover:text-[#9b59b6] hover:font-semibold cursor-pointer">
+				<div class="w-8 h-8">
+					<FaGithubAlt />
+				</div>
+			</li>
+		</ul>
+	</div>
+</header>
---
+++ apps/docs/src/lib/components/Sidebar.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let items = Array(1000).fill('item');
+</script>
+
+<aside
+	class="fixed top-[60px] border-r border-[#34495e] max-w-[300px] w-full h-screen overflow-y-auto bg-white"
+>
+	{#each items as item}
+		<div>
+			{item}
+		</div>
+	{/each}
+</aside>
