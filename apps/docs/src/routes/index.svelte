db58d69a3d3635df1ad977f9b7191439d4fd20c1
jungai <juworaphol125@gmail.com>
2022-07-01 21:15:16 +0700

    feat: add tailwindcss

--- apps/docs/src/routes/index.svelte
+++ apps/docs/src/routes/index.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-	import { Button } from 'ui';
 </script>
 
 <h1 class="text-red-400 font-semibold">Welcome to SvelteKit -> Docs</h1>
-<Button />
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
