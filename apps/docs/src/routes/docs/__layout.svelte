8d0aecdbf98c3e4ac0a0f7b067a7fad89fa31451
jungai <juworaphol125@gmail.com>
2022-07-02 20:21:53 +0700

    refactor: move to props

--- apps/docs/src/routes/docs/__layout.svelte
+++ apps/docs/src/routes/docs/__layout.svelte
@@ -1,20 +1,25 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import Header from '$lib/components/Header.svelte';
 	import Sidebar from '$lib/components/Sidebar.svelte';
+
+	$: activeRoute = $page.url.pathname;
+
+	let items = [
+		{
+			name: 'Tools',
+			basePath: '/docs/tools',
+			child: [
+				{ name: 'Taze', path: '/taze' },
+				{ name: 'Ni', path: '/ni' }
+			]
+		}
+	];
 </script>
 
 <Header />
 <main>
-	<Sidebar
-		items={[
-			{ name: 'taze', path: '/docs/tools/taze', child: [] },
-			{
-				name: 'taze',
-				path: '/docs/tools/taze',
-				child: [{ name: 'test', path: '/docs/tools/test' }]
-			}
-		]}
-	/>
+	<Sidebar {activeRoute} {items} />
 	<article
 		class="pl-[calc(var(--sidebar-width)_+_50px)] pt-[calc(var(--header-height)_+_10px)] prose"
 	>
