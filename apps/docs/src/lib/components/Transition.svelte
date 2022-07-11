c4c902b7f26d52bcdd3f09348891a08e0b59a936
jungai <juworaphol125@gmail.com>
2022-07-11 21:19:09 +0700

    feat: add transition component

---
+++ apps/docs/src/lib/components/Transition.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { fly } from 'svelte/transition';
+
+	$: {
+		console.log(url);
+	}
+
+	export let url: URL;
+</script>
+
+{#key url}
+	<div in:fly={{ y: -50, duration: 250, delay: 300 }}>
+		<slot />
+	</div>
+{/key}
