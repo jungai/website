a9b7e2df373bf5fa8896b8db1150f7e922aa8f2e
jungai <juworaphol125@gmail.com>
2022-07-11 21:19:30 +0700

    refactor: update layout & sidebar

--- apps/docs/src/lib/components/Sidebar.svelte
+++ apps/docs/src/lib/components/Sidebar.svelte
@@ -1,9 +1,38 @@
-<aside
-	class="fixed top-[var(--header-height)] z-10
-	w-sidebar-width h-screen overflow-y-auto bg-primary text-white
-	pt-6 pl-6
-	hidden md:block
-	"
->
-	<slot />
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { config } from '$config';
+	import { cls } from '$lib/utils';
+
+	let active: number = 0;
+</script>
+
+<aside class="drawer bg-base-200 w-sidebar-width fixed z-20">
+	<div class="drawer-side">
+		<div class="text-2xl font-semibold p-4">Jungai Docs</div>
+		{#each config.routes as route, index}
+			<div
+				tabindex={active}
+				class={cls(['collapse', active === index ? 'collapse-open' : null])}
+				on:click={() => (active = index)}
+			>
+				<div class="collapse-title font-medium">
+					{route.name}
+				</div>
+				{#if route.child.length > 0}
+					<div class="collapse-content">
+						<ul class="menu p-2 overflow-y-auto w-full text-base-content gap-2">
+							{#each route.child as child}
+								<li>
+									<a
+										class={cls([$page.url.pathname === child.path ? 'active' : null])}
+										href={child.path}>{child.name}</a
+									>
+								</li>
+							{/each}
+						</ul>
+					</div>
+				{/if}
+			</div>
+		{/each}
+	</div>
 </aside>
--- apps/docs/src/routes/docs/__layout.svelte
+++ apps/docs/src/routes/docs/__layout.svelte
@@ -4,22 +4,34 @@
 	import Sidebar from '$lib/components/Sidebar.svelte';
 	import Navigation from '$lib/components/Navigation.svelte';
 	import Menu from '$lib/components/Menu.svelte';
+	import FaGithubAlt from 'svelte-icons/fa/FaGithubAlt.svelte';
 	import { config } from '$config';
+	import Transition from '$lib/components/Transition.svelte';
 
 	$: activeRoute = $page.url.pathname;
 </script>
 
-<Header />
-<main>
-	<Navigation>
-		<Menu active={activeRoute} items={config.routes} />
-	</Navigation>
-	<Sidebar>
-		<Menu active={activeRoute} items={config.routes} />
-	</Sidebar>
-	<article
-		class="p-6 md:ml-[calc(var(--sidebar-width)_+_50px)] pt-[calc(var(--header-height)_+_10px)] prose"
-	>
-		<slot />
-	</article>
+<main class="relative flex">
+	<Sidebar />
+	<div class="md:ml-[calc(var(--sidebar-width)_+_50px)] w-full">
+		<div class="flex w-full justify-end py-2 px-8">
+			<ul class="flex gap-4 items-center justify-center ">
+				<li class="hover:text-secondary hover:font-semibold hover:italic cursor-pointer">
+					<a href="https://juver.xyz" target="_blank">Blog</a>
+				</li>
+				<li class="hover:text-secondary hover:font-semibold cursor-pointer">
+					<a href="https://github.com/jungai/website" target="_blank">
+						<div class="w-8 h-8">
+							<FaGithubAlt />
+						</div>
+					</a>
+				</li>
+			</ul>
+		</div>
+		<article class="prose">
+			<Transition url={$page.url}>
+				<slot />
+			</Transition>
+		</article>
+	</div>
 </main>
